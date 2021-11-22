4a210c3413020c52fa9ac33ab4d319a03e619d2c
cy-book <cz179@outlook.com>
2021-11-22 15:42:57 +0800

    fastPower not complete

--- algs/exp-6.2.py
+++ algs/exp-6.2.py
@@ -1,3 +1,15 @@
 '''
-快速幂
+快速幂 -- 重复平方乘方法
+参考 https://blog.csdn.net/weixin_46395886/article/details/113103043
 '''
+
+
+class Solution:
+    def fastPower(self, x: int, n: int, b: int) -> int:
+        ans = x
+        while n >= 1:
+            if(n % 2 == 1):
+                x *= x
+            ans = ans * x % b
+            n //= 2
+        return ans
---
+++ algs/exp-6.3.py
@@ -0,0 +1,4 @@
+'''
+蒙哥马利算法
+参考： https://blog.csdn.net/weixin_46395886/article/details/112988136
+'''
